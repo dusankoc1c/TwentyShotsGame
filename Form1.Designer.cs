2c9154d3d10a118849bdaa24ae1beb30ae521ca8
Dusan Kocic <140592423+dusankoc1c@users.noreply.github.com>
2024-05-05 17:18:26 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace TwentyShots
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.aa = new System.Windows.Forms.Label();
+            this.vreme = new System.Windows.Forms.Label();
+            this.description = new System.Windows.Forms.Label();
+            this.points = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblx = new System.Windows.Forms.Label();
+            this.lbly = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Click on the dot :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X:";
+            // 
+            // aa
+            // 
+            this.aa.AutoSize = true;
+            this.aa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aa.Location = new System.Drawing.Point(59, 26);
+            this.aa.Name = "aa";
+            this.aa.Size = new System.Drawing.Size(20, 16);
+            this.aa.TabIndex = 3;
+            this.aa.Text = "Y:";
+            // 
+            // vreme
+            // 
+            this.vreme.AutoSize = true;
+            this.vreme.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vreme.ForeColor = System.Drawing.Color.Red;
+            this.vreme.Location = new System.Drawing.Point(114, 24);
+            this.vreme.Name = "vreme";
+            this.vreme.Size = new System.Drawing.Size(18, 20);
+            this.vreme.TabIndex = 5;
+            this.vreme.Text = "0";
+            // 
+            // description
+            // 
+            this.description.AutoSize = true;
+            this.description.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.description.Location = new System.Drawing.Point(154, 16);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(77, 20);
+            this.description.TabIndex = 6;
+            this.description.Text = "good luck";
+            // 
+            // points
+            // 
+            this.points.AutoSize = true;
+            this.points.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.points.Location = new System.Drawing.Point(356, 16);
+            this.points.Name = "points";
+            this.points.Size = new System.Drawing.Size(20, 24);
+            this.points.TabIndex = 7;
+            this.points.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(412, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "in total :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(501, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblx
+            // 
+            this.lblx.AutoSize = true;
+            this.lblx.Location = new System.Drawing.Point(26, 29);
+            this.lblx.Name = "lblx";
+            this.lblx.Size = new System.Drawing.Size(13, 13);
+            this.lblx.TabIndex = 10;
+            this.lblx.Text = "0";
+            // 
+            // lbly
+            // 
+            this.lbly.AutoSize = true;
+            this.lbly.Location = new System.Drawing.Point(85, 27);
+            this.lbly.Name = "lbly";
+            this.lbly.Size = new System.Drawing.Size(13, 13);
+            this.lbly.TabIndex = 11;
+            this.lbly.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbly);
+            this.Controls.Add(this.lblx);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.points);
+            this.Controls.Add(this.description);
+            this.Controls.Add(this.vreme);
+            this.Controls.Add(this.aa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label x;
+        private System.Windows.Forms.Label y;
+        private System.Windows.Forms.Label aa;
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.Label description;
+        private System.Windows.Forms.Label points;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label vreme;
+        private System.Windows.Forms.Label lblx;
+        private System.Windows.Forms.Label lbly;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwentyShots
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int x1 = 0;
+        int y1 = 0;
+        bool _try = true; int tries = 0;
+        int Time = 5;
+        int total = 0;
+        int score = 0;
+        Random r = new Random();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Width=800;
+            Height = 800;
+            timer1.Enabled = true;
+            tries = 0;
+            Time = 5;
+            total = 0;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (tries < 20)
+            {
+                vreme.Text = Convert.ToString(Time);
+                if(Time == 5)
+                {
+                    /*Graphics Krug = CreateGraphics();
+                    SolidBrush cetka = new SolidBrush(Color.Red);
+                    Krug.FillEllipse(cetka, x1, y1, 25, 25);*/
+                    x1 = r.Next(5, 695);
+                    y1 = r.Next(100, 695);
+                    lblx.Text = Convert.ToString(x1);
+                    lbly.Text = Convert.ToString(y1);
+                    _try = true;
+                }
+                Time--;
+                if (Time < 0)
+                {
+                    tries++;
+                    Time = 5;
+                }
+            }
+            else
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (_try)
+            {
+                double Razdaljina = Math.Sqrt((e.X - x1) * (e.X - x1) + (e.Y - y1) * (e.Y - y1));
+                if (Razdaljina <= 10){ 
+                    score = 10; 
+                    description.Text = "HIT!"; 
+                }
+                else if (Razdaljina <= 20){
+                    score = 5; 
+                    description.Text = "CLOSE!"; 
+                } 
+                if (Razdaljina <= 50) 
+                { 
+                    score = 2; 
+                    description.Text = "SOMEWHERE THERE...";
+                } 
+                else if (Razdaljina <= 80)
+                { 
+                    score = 1; 
+                    description.Text = "FAR AWAY!";
+                }
+                else 
+                {
+                    description.Text = "OTHER SIDE OF THE MAP!"; 
+                    score = 0; 
+                }
+                _try = false; 
+                points.Text = Convert.ToString(score); 
+                total += score;
+                label4.Text = Convert.ToString(total); 
+            }
+        }
+    }
+}
